b0e5c0d82922f8078d8fae581288371c4ba8de2e
Grigorev Viktor <7184243@gmail.com>
2022-02-04 19:53:14 +0300

    crud - create

--- resources/views/article/index.blade.php
+++ resources/views/article/index.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
+   <small><a href="{{ route('articles.create') }}">Новая статья</a> </small>
     <h1>Список статей</h1>
     @foreach ($articles as $article)
          <a href="{{ route('articles.show', $article) }}">{{$article->name}}</a>
@@ -11,3 +12,4 @@
     @endforeach
           {{-- {{ $articles->links() }} пагинация--}}
 @endsection
+
--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,12 @@ Route::get('/', function () {
 Route::get('about', [PageController::class, 'about'])
   ->name('about'); ;
 
+Route::get('articles/create', [ArticleController::class, 'create'])
+  ->name('articles.create');
+
+Route::post('articles', [ArticleController::class, 'store'])
+  ->name('articles.store');
+
 Route::get('articles', [ArticleController::class, 'index'])
   ->name('articles.index'); 
 
